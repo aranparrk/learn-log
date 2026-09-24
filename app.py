c59491a965e9019ff9b6675cc0423a740f2a6831
aranpark <aranparrk@gmail.com>
2026-09-24 16:48:35 +0900

    🐛 fix: DB 예외 처리 및 트랜잭션 정리

--- app.py
+++ app.py
@@ -35,151 +35,246 @@ app.json.ensure_ascii = False
 # 과목 목록 조회 함수
 @app.route('/api/subjects', methods=['GET'])
 def get_subjects():
-    conn = get_connection()
-    cur = conn.cursor(pymysql.cursors.DictCursor)
+    conn = None
+    cur = None
 
-    cur.execute('SELECT * FROM subject')
+    try:
+        conn = get_connection()
+        cur = conn.cursor(pymysql.cursors.DictCursor)
 
-    subjects = cur.fetchall()
+        cur.execute('SELECT * FROM subject')
 
-    cur.close()
-    conn.close()
+        subjects = cur.fetchall()
 
-    return jsonify(subjects)
+        return jsonify(subjects)
+    except pymysql.MySQLError as e:
+        print(e)
+
+        return jsonify({
+            'message': 'get_subjects() DB 처리 실패'
+        }), 500
+    finally:
+        if cur is not None:
+            cur.close()
+
+        if conn is not None:
+            conn.close()
 
 # 공부 목록 조회 함수
 @app.route('/api/studies', methods=['GET'])
 def get_studies():
-    conn = get_connection()
-    cur = conn.cursor(pymysql.cursors.DictCursor)
+    conn = None
+    cur = None
 
-    cur.execute("""
-        SELECT
-            study.id,
-            subject.name AS subject,
-            study.study_date,
-            study.study_minute,
-            study.content
-        FROM study
-        JOIN subject
-            ON study.subject_id = subject.id
-    """)
+    try:
+        conn = get_connection()
+        cur = conn.cursor(pymysql.cursors.DictCursor)
 
-    studies = cur.fetchall()
+        cur.execute("""
+            SELECT
+                study.id,
+                subject.name AS subject,
+                study.study_date,
+                study.study_minute,
+                study.content
+            FROM study
+            JOIN subject
+                ON study.subject_id = subject.id
+        """)
 
-    cur.close()
-    conn.close()
+        studies = cur.fetchall()
 
-    return jsonify(studies)
+        return jsonify(studies)
+    except pymysql.MySQLError as e:
+        print(e)
+
+        return jsonify({
+            'message': 'get_studies() DB 처리 실패'
+        }), 500
+    finally:
+        if cur is not None:
+            cur.close()
+
+        if conn is not None:
+            conn.close()
+
+
 
 # 과목 등록 함수
 @app.route('/api/subjects', methods=['POST'])
 def create_subject():
-    data = request.get_json()
+    conn = None
+    cur = None
 
-    subject_name = data['name']
+    try:
+        data = request.get_json()
 
-    conn = get_connection()
-    cur = conn.cursor()
+        subject_name = data['name']
 
-    cur.execute(
-        '''
-        INSERT INTO subject(name)
-        VALUES (%s)
-        ''',
-        (subject_name,)
-    )
+        conn = get_connection()
+        cur = conn.cursor()
 
-    conn.commit()
-    cur.close()
-    conn.close()
+        cur.execute(
+            '''
+            INSERT INTO subject(name)
+            VALUES (%s)
+            ''',
+            (subject_name,)
+        )
 
-    return jsonify({
-        'message' : '등록성공',
-        'subject': data
-    }), 201
+        conn.commit()
+
+        return jsonify({
+            'message' : '과목 등록 성공',
+            'subject': data
+        }), 201
+    except pymysql.MySQLError as e:
+        if conn is not None:
+            conn.rollback()
+
+        print(e)
+
+        return jsonify({
+            'message': 'create_subject() DB 처리 실패'
+        }), 500
+    finally:
+        if cur is not None:
+            cur.close()
+
+        if conn is not None:
+            conn.close()
 
 # 공부 등록 함수
 @app.route('/api/studies', methods=['POST'])
 def create_study():
-    data = request.get_json()
+    conn = None
+    cur = None
 
-    subject_id = data['subject_id']
-    study_date = data['study_date']
-    study_minute = data['study_minute']
-    content = data['content']
+    try:
+        data = request.get_json()
 
-    conn = get_connection()
-    cur = conn.cursor()
+        subject_id = data['subject_id']
+        study_date = data['study_date']
+        study_minute = data['study_minute']
+        content = data['content']
 
-    cur.execute(
-        '''
-            INSERT INTO study(subject_id, study_date, study_minute, content)
-            VALUES (%s, %s, %s, %s)
-        ''',
-        (subject_id, study_date, study_minute, content)
-    )
+        conn = get_connection()
+        cur = conn.cursor()
 
-    conn.commit()
-    cur.close()
-    conn.close()
+        cur.execute(
+            '''
+                INSERT INTO study(subject_id, study_date, study_minute, content)
+                VALUES (%s, %s, %s, %s)
+            ''',
+            (subject_id, study_date, study_minute, content)
+        )
 
-    return jsonify({
-        'message': '등록성공',
-        'study': data
-    }), 201
+        conn.commit()
+
+        return jsonify({
+            'message': '공부 등록 성공',
+            'study': data
+        }), 201
+    except pymysql.MySQLError as e:
+        if conn is not None:
+            conn.rollback()
+
+        print(e)
+
+        return jsonify({
+            'message': 'create_study() DB 처리 실패'
+        }), 500
+    finally:
+        if cur is not None:
+            cur.close()
+        if conn is not None:
+            conn.close()
 
 # 공부 목록 삭제
 @app.route('/api/studies/<int:study_id>', methods=['DELETE'])
 def delete_study(study_id):
+    conn = None
+    cur = None
 
-    conn = get_connection()
-    cur = conn.cursor()
+    try:
+        conn = get_connection()
+        cur = conn.cursor()
 
-    cur.execute(
-        '''
-            DELETE FROM study WHERE id = %s
-        ''',
-        (study_id,)
-    )
+        cur.execute(
+            '''
+                DELETE FROM study WHERE id = %s
+            ''',
+            (study_id,)
+        )
 
-    conn.commit()
-    cur.close()
-    conn.close()
+        conn.commit()
 
-    return jsonify({
-        'message' : '삭제 성공',
-        'study': study_id
-    }), 200
+
+
+        return jsonify({
+            'message' : '공부 삭제 성공',
+            'study': study_id
+        }), 200
+    except pymysql.MySQLError as e:
+        if conn is not None:
+            conn.rollback()
+
+        print(e)
+
+        return jsonify({
+            'message': 'delete_study() DB 처리 실패'
+        }), 500
+    finally:
+        if cur is not None:
+            cur.close()
+
+        if conn is not None:
+            conn.close()
 
 # 공부 목록 수정
 @app.route('/api/studies/<int:study_id>', methods=['PUT'])
 def update_study(study_id):
-    data = request.get_json()
+    conn = None
+    cur = None
 
-    conn = get_connection()
-    cur = conn.cursor()
+    try:
+        data = request.get_json()
 
-    subject_id = data['subject_id']
-    study_date = data['study_date']
-    study_minute = data['study_minute']
-    content = data['content']
+        conn = get_connection()
+        cur = conn.cursor()
 
-    cur.execute(
-        '''
-            UPDATE study SET subject_id = %s, study_date = %s, study_minute = %s, content = %s WHERE id = %s
-        ''',
-        (subject_id, study_date, study_minute, content, study_id)
-    )
+        subject_id = data['subject_id']
+        study_date = data['study_date']
+        study_minute = data['study_minute']
+        content = data['content']
 
-    conn.commit()
-    cur.close()
-    conn.close()
+        cur.execute(
+            '''
+                UPDATE study SET subject_id = %s, study_date = %s, study_minute = %s, content = %s WHERE id = %s
+            ''',
+            (subject_id, study_date, study_minute, content, study_id)
+        )
 
-    return jsonify({
-        'message' : '수정 성공',
-        'study': data
-    }), 200
+        conn.commit()
+
+        return jsonify({
+            'message' : '공부 수정 성공',
+            'study': data
+        }), 200
+    except pymysql.MySQLError as e:
+        if conn is not None:
+            conn.rollback()
+
+        print(e)
+
+        return jsonify({
+            'message': 'update_study() DB 처리 실패'
+        }), 500
+    finally:
+        if cur is not None:
+            cur.close()
+        if conn is not None:
+            conn.close()
 
 # 기본 라우트
 @app.route('/')
